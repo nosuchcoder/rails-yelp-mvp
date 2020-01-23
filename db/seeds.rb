4d698def60b7a7a3dee4e3a656aaf48f38a9642c
Kwame Dalrymple <neokwame@tutanota.com>
2020-01-23 20:18:12 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: "RamenHouse",
+    address: "55 Garland Street, America,",
+    phone_number: "02079865098",
+    category: "japanese",
+  }, 
+  {
+    name: "TheBaja",
+    address: "95 halfway lane, UK",
+    phone_number: "02079865097",
+    category: "belgian",
+  },
+  {
+    name: "LaFrog",
+    address: "99 boudville lane, France",
+    phone_number: "02079865097",
+    category: "french",
+  },
+  {
+    name: "Rockyhouse",
+    address: "Rocky street ave, Italy",
+    phone_number: "02079865077",
+    category: "italian",
+  },
+  {
+    name: "ChowDownMein",
+    address: "ChinaTown, London",
+    phone_number: "02079865077",
+    category: "chinese",
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
